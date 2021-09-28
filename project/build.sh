798175984794ceb780ebd26a199b6b4c8346d34e
Anant Jain <anant.j2409@gmail.com>
2021-09-28 13:00:19 -0400

    Updated build script

--- project/build.sh
+++ project/build.sh
@@ -1,5 +1,9 @@
 # Only for use within EC2 instance (Builds out of current project directory)
+chmod u+x build.sh
 git pull
+chmod u+x build.sh
+chmod u+x clean.sh
 ./clean.sh
+yarn
 yarn build
 cp -r dist/* ../../
