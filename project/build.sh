33af5bcd65b8b8e2eca4d62b8414d11ccf0d5742
Anant Jain <anant.j2409@gmail.com>
2021-09-28 12:39:56 -0400

    Updated build scripts for EC2 Server

--- project/build.sh
+++ project/build.sh
@@ -1,3 +1,4 @@
+# Only for use within EC2 instance (Builds out of current project directory)
 ./clean.sh
 yarn build
-cp -r dist/* ../
+cp -r dist/* ../../
--- project/clean.sh
+++ project/clean.sh
@@ -1,6 +1,5 @@
 rm -rf dist
-rm -rf ../css
-rm -rf ../js
-rm -rf ../favicon.ico
-rm -rf ../index.html
-rm -rf dist
+rm -rf ../../css
+rm -rf ../../js
+rm -rf ../../favicon.ico
+rm -rf ../../index.html
