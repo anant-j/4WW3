574d474dacd8a2c5a3a9373d2b686fe46d158067
Anant Jain <anant.j2409@gmail.com>
2021-09-30 19:16:16 -0400

    Added footer

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,42 +1,21 @@
 <template>
-  <div>
+  <div id="outerdiv">
     <Navbar />
     <router-view />
+    <Footer id="footer" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    Navbar
-  }
+    Navbar,
+    Footer,
+  },
 };
 </script>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ project/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container-fluid">
+    <footer
+      class="
+        d-flex
+        flex-wrap
+        justify-content-between
+        align-items-center
+        py-3
+        my-4
+        border-top
+      "
+    >
+      <div class="col-md-4 d-flex align-items-center">
+        <a
+          href="/"
+          class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+        >
+          <svg class="bi" width="30" height="24">
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+        </a>
+        <span class="text-muted"
+          >© {{ new Date().getFullYear() }} OuseMouse Inc (4WW3)</span
+        >
+      </div>
+
+      All rights reserved. Developed by Anant Jain & Kuber Khanna.
+    </footer>
+  </div>
+</template>
