af1020c8518b1d7383ab48a74411448c151fa26d
Anant Jain <anant.j2409@gmail.com>
2021-10-05 11:31:47 -0400

    Fixing restaurant page

--- app/pages/restaurant.vue
+++ app/pages/restaurant.vue
@@ -1,9 +1,42 @@
 <template>
   <div>
-    <Map id="restaurantMap" />
+    <div id="restaurantTopBar" class="row">
+      <Map class="col-md-6 mb-4" />
+      <img
+        class="col-md-6"
+        src="https://dynamicmedia.zuza.com/zz/m/original_/7/2/7266f0c7-32b2-4514-af18-1b9dd948b072/Osmow%20s%2004_Super_Portrait.jpg"
+      />
+    </div>
     <div class="container mt-5">
       <h3 class="pb-4 mb-4 border-bottom text-center">Osmows</h3>
       <div class="row g-5">
+        <div class="col-md-4">
+          <div class="position-sticky top2rem">
+            <div class="p-4 bg-light rounded">
+              <h4 class="fst-italic">About</h4>
+              <p class="mb-0">
+                Osmow's is a fast casual Mediterranean restaurant capturing a
+                truly modern taste while giving customers the luxury of
+                customizing their preferences.
+              </p>
+              <br />
+              <h4 class="fst-italic">Location</h4>
+              <p class="mb-0">Hamilton, Ontario</p>
+              <br />
+              <h4 class="fst-italic">Hours</h4>
+              <p class="mb-0">Monday - 10:00 A.M - 9:00 P.M</p>
+              <p class="mb-0">Tuesday - 10:00 A.M - 9:00 P.M</p>
+              <p class="mb-0">Wednesday - 10:00 A.M - 9:00 P.M</p>
+              <p class="mb-0">Thursday - 10:00 A.M - 9:00 P.M</p>
+              <p class="mb-0">Friday - 10:00 A.M - 9:00 P.M</p>
+              <p class="mb-0">Saturday - 10:00 A.M - 9:00 P.M</p>
+              <p class="mb-0">Sunday - 10:00 A.M - 9:00 P.M</p>
+              <br />
+              <h4 class="fst-italic">Website</h4>
+              <a href="https://mrosmow.com/" target="_">https://mrosmow.com/</a>
+            </div>
+          </div>
+        </div>
         <div class="col-md-8">
           <h3>9 Reviews</h3>
           <hr />
@@ -53,34 +86,6 @@
           <p>⭐⭐⭐⭐</p>
           <p>Would buy again.</p>
           <hr />
-        </div>
-
-        <div class="col-md-4">
-          <div class="position-sticky top2rem">
-            <div class="p-4 bg-light rounded">
-              <h4 class="fst-italic">About</h4>
-              <p class="mb-0">
-                Osmow's is a fast casual Mediterranean restaurant capturing a
-                truly modern taste while giving customers the luxury of
-                customizing their preferences.
-              </p>
-              <br />
-              <h4 class="fst-italic">Location</h4>
-              <p class="mb-0">Hamilton, Ontario</p>
-              <br />
-              <h4 class="fst-italic">Hours</h4>
-              <p class="mb-0">Monday - 10:00 A.M - 9:00 P.M</p>
-              <p class="mb-0">Tuesday - 10:00 A.M - 9:00 P.M</p>
-              <p class="mb-0">Wednesday - 10:00 A.M - 9:00 P.M</p>
-              <p class="mb-0">Thursday - 10:00 A.M - 9:00 P.M</p>
-              <p class="mb-0">Friday - 10:00 A.M - 9:00 P.M</p>
-              <p class="mb-0">Saturday - 10:00 A.M - 9:00 P.M</p>
-              <p class="mb-0">Sunday - 10:00 A.M - 9:00 P.M</p>
-              <br />
-              <h4 class="fst-italic">Website</h4>
-              <a href="https://mrosmow.com/" target="_">https://mrosmow.com/</a>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -137,4 +142,3 @@ export default {
   },
 }
 </script>
-
