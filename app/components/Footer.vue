c4cc3bddc2842565aedabe0a8cea5ace9e1659b3
Anant Jain <anant.j2409@gmail.com>
2021-10-06 16:36:11 -0400

    Fixed footer for accessibility

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -10,9 +10,7 @@
       class="d-flex flex-wrap justify-content-between align-items-center py-2"
     >
       <div class="col-md-4 d-flex align-items-center">
-        <span class="text-muted"
-          >© {{ new Date().getFullYear() }} OuseMouse Inc (4WW3)</span
-        >
+        <span> © {{ new Date().getFullYear() }} OuseMouse Inc (4WW3)</span>
       </div>
 
       All rights reserved. Developed by Anant Jain and Kuber Khanna.
