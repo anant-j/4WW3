7b54da1eab2cb4c38326bb8391502290dd69bd46
khannk1 <35265519+khannk1@users.noreply.github.com>
2021-10-06 20:53:10 -0400

    Adding alt text to images (#2)

--- app/components/Map.vue
+++ app/components/Map.vue
@@ -3,6 +3,7 @@
     <img
       id="mapImg"
       src="https://www.hamiltonhealthsciences.ca/wp-content/uploads/2018/12/mcmaster_university_medical_centre.jpg"
+      alt="Location Map"
       loading="lazy"
     />
   </div>
