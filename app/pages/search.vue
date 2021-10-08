b788825fc71b285006bb39304057853672a8a88c
kuber khanna <khannakuber14@gmail.com>
2021-10-08 12:05:49 -0400

    Added comments for search results

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -1,17 +1,21 @@
 <template>
+  <!-- This is the results page that is displayed after a search has been made -->
   <div class="row container-fluid mt-3">
+    <!-- Display the map component with results -->
     <Map class="col mb-1" />
+    <!-- Display the list component with results -->
     <List class="col-md-3" />
   </div>
 </template>
 
 <script>
-import List from '@/components/List.vue'
-import Map from '@/components/Map.vue'
+import List from "@/components/List.vue";
+import Map from "@/components/Map.vue";
 export default {
   components: {
+    // Registering components
     List,
     Map,
   },
-}
+};
 </script>
