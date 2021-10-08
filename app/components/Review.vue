5c606ea77847d668f6bb996e14326e0d7d625b77
Anant Jain <anant.j2409@gmail.com>
2021-10-08 17:36:41 -0400

    Adding review component

---
+++ app/components/Review.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="row">
+      <img
+        class="col-3 col-md-2 user-image"
+        :src="imageurl"
+        :alt="username"
+        loading="lazy"
+      />
+      <div class="col-9 col-md-10">
+        <h4>{{ title }}</h4>
+        <span v-for="star in parseInt(rating)" :key="star">⭐</span>
+        <br />
+        <p>{{ username }}</p>
+      </div>
+    </div>
+    <br />
+    <p>
+      {{ review }}
+    </p>
+    <hr />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    imageurl: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    review: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
