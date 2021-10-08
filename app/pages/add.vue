01ef08306fa08af24ecc8141b56c769efc515d2b
kuber khanna <khannakuber14@gmail.com>
2021-10-08 11:36:17 -0400

    Adding comments for add.vue

--- app/pages/add.vue
+++ app/pages/add.vue
@@ -1,7 +1,9 @@
 <template>
+  <!-- This is the page that is displayed when a user wants to add a restaurant -->
   <div class="container mt-3 align-form">
     <h1>Add a Restaurant</h1>
     <form class="align-items-center">
+      <!-- The div below takes the name of the restaurant -->
       <div class="row justify-content-center">
         <div class="col-md-6">
           <label for="name">Restaurant Name</label>
@@ -13,6 +15,7 @@
           />
         </div>
       </div>
+      <!-- The div below takes the description/about info of the restaurant -->
       <div class="row justify-content-center">
         <div class="col-md-6">
           <label for="description">Restaurant Description</label>
@@ -25,6 +28,7 @@
         </div>
       </div>
       <div class="row justify-content-center">
+        <!-- The div below takes the latitude of the restaurant -->
         <div class="col-md-3">
           <label for="latitude">Latitude</label>
           <input
@@ -34,6 +38,7 @@
             placeholder="Enter Latitude"
           />
         </div>
+        <!-- The div below takes the longitude of the restaurant -->
         <div class="col-md-3">
           <label for="longitude">Longitude</label>
           <input
@@ -44,6 +49,7 @@
           />
         </div>
       </div>
+      <!-- The div below allows the user to upload an image of the restaurant -->
       <div class="row justify-content-center mt-3">
         <div class="col-md-6">
           <label for="image">Upload an Image :</label>&nbsp;
@@ -55,6 +61,7 @@
           />
         </div>
       </div>
+      <!-- The div below allows the user to upload a video of the restaurant -->
       <div class="row justify-content-center">
         <div class="col-md-6">
           <label for="video">Upload a Video &nbsp;&nbsp;&nbsp;:</label>&nbsp;
