90591cd951572084808cad5620a630d7723363da
Anant Jain <anant.j2409@gmail.com>
2021-10-07 22:27:43 -0400

    Adding comments in and linting default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- The div below is the outermost view of the application -->
+  <!-- We use Vue's class binding to assign the respective classes based on the night mode status -->
   <div
     class="wrapper"
     :class="{
@@ -9,14 +11,18 @@
     }"
   >
     <header>
+      <!-- Here, we are importing the Navbar component, which will always persist above all views -->
       <Navbar />
     </header>
     <article>
+      <!-- Added a transistion frame which plays an out-in fade transistion every time Nuxt view changes-->
       <transition name="fade">
+        <!-- The view (different components under app/pages) will be rendered inside the div below based on the selection from NuxtLink in Navbar or other components-->
         <Nuxt />
       </transition>
     </article>
     <footer>
+      <!-- Here, we are importing the Navbar component, which will always persist below all views -->
       <Footer />
     </footer>
   </div>
@@ -26,8 +32,7 @@
 import Footer from '~/components/Footer.vue'
 import Navbar from '~/components/Navbar.vue'
 export default {
-  components: { Navbar, Footer },
-  transition: 'fade',
+  components: { Navbar, Footer }, // Registering components
+  transition: 'fade', // Registering transistion
 }
 </script>
-
