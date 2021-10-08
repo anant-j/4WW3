609f784920b7def110ecdc43553a94111e0a9770
Anant Jain <anant.j2409@gmail.com>
2021-10-08 12:37:14 -0400

    Improved accessibility for the about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -5,8 +5,14 @@
       This is a website developed for CS4WW3 Fall 2021 by Anant Jain and Kuber
       Khanna.
     </p>
-    <a class="github-url" href="https://github.com/anant-j/AK-4WW3" target="_blank">
-           <font-awesome-icon :icon="['fab', 'github']" />
-           </a>
+    <a
+      class="github-url"
+      href="https://github.com/anant-j/AK-4WW3"
+      target="_blank"
+      aria-label="CS4WW3 Github"
+      rel="noreferrer"
+    >
+      <font-awesome-icon :icon="['fab', 'github']" />
+    </a>
   </div>
 </template>
