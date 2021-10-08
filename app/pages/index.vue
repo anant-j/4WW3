d1bf29d1bddc958ab91906553c3b9a806a8d0fba
kuber khanna <khannakuber14@gmail.com>
2021-10-08 11:44:51 -0400

    Adding comments for index.vue/Homepage/default view

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,19 +1,23 @@
 <template>
+  <!-- This is the home view that is displayed when the website/application is initially rendered. This is the default view on default path (www.domain.com/)  -->
   <div class="container align-form">
+    <!-- Website Title -->
     <h1 class="heading">OuseMouse</h1>
-    <!-- <div class="text-center align-items-center justify-content-center"> -->
+    <!-- Website Description -->
     <p class="text-center">
       Using OuseMouse, you can search a restaurant for reviews and ratings, add
       your own reviews, and even add your favourite restaurants for others to
       review!
     </p>
-    <!-- </div> -->
     <hr />
+    <!-- Search area starts below-->
     <h2>Search for a Restaurant</h2>
     <form class="align-items-center">
+      <!-- Search area starts below-->
       <div class="row justify-content-center">
         <div class="col-md-6">
           <label for="searchText">Restaurant's Name</label>
+          <!-- The input field takes the name/keywords to search for restaurants. It gets disabled if a rating has been selected from the dropdown below this field -->
           <input
             id="searchText"
             v-model="searchText"
@@ -31,6 +35,7 @@
       <div class="row justify-content-center">
         <div class="col-md-6">
           <label for="searchRating">Search By Ratings</label>
+          <!-- The select field takes the ratings to search for restaurant. It gets disabled if a keyword has been entered in the text box above this field -->
           <select
             id="searchRating"
             v-model="selectedRating"
@@ -58,31 +63,33 @@
 export default {
   data() {
     return {
-      searchText: '',
-      selectedRating: 0,
-    }
+      searchText: "", // Data property for the search text entered
+      selectedRating: 0, // Data property for the rating selected
+    };
   },
   watch: {
     searchText(oldVal, newVal) {
-      if (newVal === '') {
-        this.selectedRating = 0
+      if (newVal === "") {
+        this.selectedRating = 0; // If search text is entered, make the selected rating 0.
       }
     },
   },
   methods: {
     search() {
+      // This method adds page redirection with query fields to search for the restaurant.
+      // Currently, this is dumb redirection logic and will not impact the results page in any manner.
       if (this.selectedRating) {
         this.$router.push({
-          path: 'search',
+          path: "search",
           query: { rating: this.selectedRating },
-        })
+        });
       } else {
         this.$router.push({
-          path: 'search',
-          query: { keyword: this.searchText || 'random' },
-        })
+          path: "search",
+          query: { keyword: this.searchText || "random" },
+        });
       }
     },
   },
-}
+};
 </script>
