ee16838a40937296ebd0a546d605f6d005b5d09f
Anant Jain <anant.j2409@gmail.com>
2021-10-07 22:16:18 -0400

    Adding comments in Footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- The div below represents the footer that is used throughout the website -->
+  <!-- We use Vue's class binding to assign the respective classes based on the night mode status -->
   <div
     class="container-fluid text-light"
     :class="{
@@ -10,9 +12,8 @@
       class="d-flex flex-wrap justify-content-between align-items-center py-2"
     >
       <div class="col-md-4 d-flex align-items-center">
-        <span> © {{ new Date().getFullYear() }} OuseMouse Inc (4WW3)</span>
+        <span> © 2021 OuseMouse Inc (4WW3)</span>
       </div>
-
       All rights reserved. Developed by Anant Jain and Kuber Khanna.
     </footer>
   </div>
