438fff0a83c8af6ce5a6d93a0c138134b6713881
kuber khanna <khannakuber14@gmail.com>
2021-10-08 12:03:02 -0400

    Added comments for restaurant page

--- app/pages/restaurant.vue
+++ app/pages/restaurant.vue
@@ -1,7 +1,10 @@
 <template>
+  <!-- The view below represents the individual object page for the restaurant -->
+  <!-- This component has been borrowed from the Bootstrap component example library and then modified to satisfy our requirements (https://getbootstrap.com/docs/4.0/examples/blog/) -->
   <div>
     <div id="restaurant-top-bar" class="row">
       <Map class="col-md-6 mb-4" />
+      <!-- Restaurant's image -->
       <img
         class="col-md-6"
         src="https://dynamicmedia.zuza.com/zz/m/original_/7/2/7266f0c7-32b2-4514-af18-1b9dd948b072/Osmow%20s%2004_Super_Portrait.jpg"
@@ -10,6 +13,7 @@
       />
     </div>
     <div class="container mt-5">
+      <!-- Restaurant's title -->
       <h3 class="pb-4 mb-4 border-bottom text-center">Osmows</h3>
       <div class="row g-5">
         <div class="col-md-4">
@@ -21,6 +25,7 @@
                 'border-dark': !$store.state.nightMode,
               }"
             >
+              <!-- Restaurant's about info -->
               <section id="about">
                 <h4 class="fst-italic">About</h4>
                 <p class="mb-0">
@@ -30,11 +35,13 @@
                 </p>
               </section>
               <br />
+              <!-- Restaurant's location info -->
               <section id="location">
                 <h4 class="fst-italic">Location</h4>
                 <p class="mb-0">Hamilton, Ontario</p>
               </section>
               <br />
+              <!-- Restaurant's operating hours  -->
               <section id="hours">
                 <h4 class="fst-italic">Hours</h4>
                 <p class="mb-0">Monday - 10:00 A.M - 9:00 P.M</p>
@@ -46,6 +53,7 @@
                 <p class="mb-0">Sunday - 10:00 A.M - 9:00 P.M</p>
               </section>
               <br />
+              <!-- Restaurant's website  -->
               <section id="website">
                 <h4 class="fst-italic">Website</h4>
                 <a href="https://mrosmow.com/" class="plain-link" target="_"
@@ -57,9 +65,12 @@
         </div>
         <div class="col-md-8">
           <div class="row">
+            <!-- Number of reviews -->
             <h4 class="col">9 Reviews</h4>
+            <!-- Add your own review Modal component -->
             <SubmitReview class="col" />
           </div>
+          <!-- Restaurant reviews-->
           <hr />
           <h4>Perfect place for dinner</h4>
           <p>⭐⭐⭐⭐</p>
@@ -114,60 +125,56 @@
 </template>
 
 <script>
-import Map from '@/components/Map.vue'
-import SubmitReview from '@/components/SubmitReview.vue'
+import Map from "@/components/Map.vue";
+import SubmitReview from "@/components/SubmitReview.vue";
 export default {
   components: {
+    // Registering components
     Map,
     SubmitReview,
   },
-  data() {
-    return {
-      submitReviewEnabled: false,
-    }
-  },
-
   head: {
-    title: 'Osmows',
+    // Meta tag descriptions for Facebook's Open Graph Protocol and Twitter Cards
+    title: "Osmows",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        name: 'og:title',
-        content: 'Osmows',
+        name: "og:title",
+        content: "Osmows",
       },
       {
-        name: 'og:type',
-        content: 'Website',
+        name: "og:type",
+        content: "Website",
       },
       {
-        name: 'og:image',
+        name: "og:image",
         content:
-          'https://mrosmow.com/wp-content/uploads/2019/01/Osmows-Straight-Logo.png',
+          "https://mrosmow.com/wp-content/uploads/2019/01/Osmows-Straight-Logo.png",
       },
       {
-        name: 'og:url',
-        content: 'https://mrosmow.com/',
+        name: "og:url",
+        content: "https://mrosmow.com/",
       },
       {
-        name: 'twitter:card',
-        content: 'summary_large_image',
+        name: "twitter:card",
+        content: "summary_large_image",
       },
       {
-        name: 'twitter:title',
-        content: 'Osmows',
+        name: "twitter:title",
+        content: "Osmows",
       },
       {
-        name: 'twitter:description',
+        name: "twitter:description",
         content:
           "Osmow's is a fast casual Mediterranean restaurant capturing a truly modern taste while giving customers the luxury of customizing their preferences.",
       },
       {
-        name: 'twitter:image',
+        name: "twitter:image",
         content:
-          'https://mrosmow.com/wp-content/uploads/2019/01/Osmows-Straight-Logo.png',
+          "https://mrosmow.com/wp-content/uploads/2019/01/Osmows-Straight-Logo.png",
       },
     ],
   },
-}
+};
 </script>
