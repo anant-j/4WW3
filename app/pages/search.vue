52240d1e253304a02834a27c68d104cc3a6314c0
Anant Jain <anant.j2409@gmail.com>
2021-10-27 11:51:53 -0400

    Fixed map height for search page

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -1,8 +1,10 @@
 <template>
   <!-- This is the results page that is displayed after a search has been made -->
   <div class="row container-fluid mt-3">
+    <div class="col mb-1 searchMap" >
     <!-- Display the map component with results -->
-    <Map class="col mb-1" />
+    <Map class="col mb-1 h-100" />
+    </div>
     <!-- Display the list component with results -->
     <List class="col-md-3" />
   </div>
