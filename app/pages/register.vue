be83017404ebaccaf4928d861a9621c3481ec11f
kuber khanna <khannakuber14@gmail.com>
2021-10-29 12:26:58 -0400

    Adding recenter algo

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -3,128 +3,214 @@
   <div class="container mt-3 align-form">
     <h1>Create Account</h1>
     <form class="align-items-center">
-      <div class="row justify-content-center">
-        <div class="col-md-3">
-          <label for="firstname">First Name</label>
-          <input
-            id="firstname"
-            v-model="firstname"
-            type="text"
-            class="form-control"
-            placeholder="First Name"
-            :class="{
-              'is-invalid': !validate().firstname && blur,
-              'is-valid': validate().firstname && blur,
-            }"
-          />
-          <div class="invalid-feedback">Please enter a first name.</div>
-        </div>
-        <div class="col-md-3">
-          <label for="lastname">Last Name</label>
-          <input
-            id="lastname"
-            v-model="lastname"
-            type="text"
-            class="form-control"
-            placeholder="Last Name"
-            :class="{
-              'is-invalid': !validate().lastname && blur,
-              'is-valid': validate().lastname && blur,
-            }"
-          />
-          <div class="invalid-feedback">Please enter a last name.</div>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <label for="email">Email</label>
-          <!-- This is the HTML5 email field -->
-          <input
-            id="email"
-            v-model="email"
-            type="email"
-            class="form-control"
-            placeholder="Enter Email"
-            :class="{
-              'is-invalid': !validate().email && blur,
-              'is-valid': validate().email && blur,
-            }"
-          />
-        <div class="invalid-feedback">Please enter a valid email.</div>
-        </div>
-
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <label for="password">Password</label>
-          <!-- This is the HTML5 password field -->
-          <input
-            id="password"
-            v-model="password"
-            type="password"
-            class="form-control"
-            placeholder="Enter Password"
-            :class="{
-              'is-invalid': !validate().password && blur,
-              'is-valid': validate().password && blur,
-            }"
-          />
-        <div class="invalid-feedback">
-          Please enter a password that is at least 5 characters long, and
-          contains a number, a symbol and a letter.
-        </div>
-
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <label for="dob">Date of Birth</label>
-          <!-- This is the HTML5 date field for selecting user's Date of Birth -->
-          <input
-            id="dob"
-            v-model="dob"
-            type="date"
-            class="form-control"
-            :max="getMaxDate()"
-            :class="{
-              'is-invalid': !validate().dob && blur,
-              'is-valid': validate().dob && blur,
-            }"
-          />
-        <div class="invalid-feedback">Please enter a valid DOB</div>
-        </div>
-
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-sm-6 col-md-3">
-          <!-- This is the dropdown field for selecting user's location -->
-          <select class="form-select" aria-label="Default select example">
-            <option selected>Select Location</option>
-            <option value="1">Hamilton</option>
-            <option value="2">Toronto</option>
-            <option value="3">Ancaster</option>
-            <option value="3">Vancouver</option>
-          </select>
-        </div>
-        <div class="col-sm-3 col-md-3">
-          <div class="form-check">
-            <!-- This is the checkbox field for remembering user's location -->
-            <input id="rememberLocation" class="form-check-input" type="checkbox" value checked />
-            <label class="form-check-label" for="rememberLocation">Remember My Location</label>
+      <div v-show="page == 1">
+        <div class="row justify-content-center">
+          <div class="col-md-3">
+            <label for="firstname">First Name</label>
+            <input
+              id="firstname"
+              v-model="firstname"
+              type="text"
+              class="form-control"
+              placeholder="First Name"
+              :class="{
+                'is-invalid': !validate().firstname && blur,
+                'is-valid': validate().firstname && blur,
+              }"
+            />
+            <div class="invalid-feedback">Please enter a first name.</div>
+          </div>
+          <div class="col-md-3">
+            <label for="lastname">Last Name</label>
+            <input
+              id="lastname"
+              v-model="lastname"
+              type="text"
+              class="form-control"
+              placeholder="Last Name"
+              :class="{
+                'is-invalid': !validate().lastname && blur,
+                'is-valid': validate().lastname && blur,
+              }"
+            />
+            <div class="invalid-feedback">Please enter a last name.</div>
           </div>
         </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <label for="email">Email</label>
+            <!-- This is the HTML5 email field -->
+            <input
+              id="email"
+              v-model="email"
+              type="email"
+              class="form-control"
+              placeholder="Enter Email"
+              :class="{
+                'is-invalid': !validate().email && blur,
+                'is-valid': validate().email && blur,
+              }"
+            />
+            <div class="invalid-feedback">Please enter a valid email.</div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <label for="password">Password</label>
+            <!-- This is the HTML5 password field -->
+            <input
+              id="password"
+              v-model="password"
+              type="password"
+              class="form-control"
+              placeholder="Enter Password"
+              :class="{
+                'is-invalid': !validate().password && blur,
+                'is-valid': validate().password && blur,
+              }"
+            />
+            <div class="invalid-feedback">
+              Please enter a password that is at least 5 characters long, and
+              contains a number, a symbol and a letter.
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <label for="dob">Date of Birth</label>
+            <!-- This is the HTML5 date field for selecting user's Date of Birth -->
+            <input
+              id="dob"
+              v-model="dob"
+              type="date"
+              class="form-control"
+              :max="getMaxDate()"
+              :class="{
+                'is-invalid': !validate().dob && blur,
+                'is-valid': validate().dob && blur,
+              }"
+            />
+            <div class="invalid-feedback">Please enter a valid DOB</div>
+          </div>
+        </div>
+        <div class="row justify-content-center mt-3">
+          <button type="button" class="col-3 btn btn-primary" @click="submit">
+            Next
+          </button>
+        </div>
+        <div class="row justify-content-center mt-3">
+          <!-- Login button that is only enabled if both email and password fields are empty -->
+          <button
+            type="button"
+            class="col-3 btn btn-outline-info"
+            :disabled="email != '' || password != ''"
+            @click="openPage()"
+          >
+            Go to Login Page
+          </button>
+        </div>
       </div>
-      <div class="row justify-content-center mt-3">
-        <button type="button" class="col-3 btn btn-primary" @click="submit">Submit</button>
-      </div>
-      <div class="row justify-content-center mt-3">
-        <!-- Login button that is only enabled if both email and password fields are empty -->
-        <button
-          type="button"
-          class="col-3 btn btn-outline-info"
-          :disabled="email != '' || password != ''"
-          @click="openPage()"
-        >Login Page</button>
+      <div v-show="page == 2">
+        <div v-if="searchQueryEnabled" class="row justify-content-center">
+          <div class="col-md-6">
+            <label for="loationSearch">Search Address</label>
+            <!-- This is the HTML5 email field -->
+            <input
+              id="loationSearch"
+              v-model="searchQuery"
+              type="text"
+              class="form-control"
+              placeholder="Start typing to auto fill address"
+            />
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <label for="street">Street</label>
+            <!-- This is the HTML5 email field -->
+            <input
+              id="street"
+              v-model="street"
+              type="text"
+              class="form-control"
+              placeholder="Street"
+              :disabled="searchQueryEnabled"
+            />
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-3">
+            <label for="city">City</label>
+            <input
+              id="city"
+              v-model="city"
+              type="text"
+              class="form-control"
+              placeholder="City"
+              :disabled="searchQueryEnabled"
+            />
+          </div>
+          <div class="col-md-3">
+            <label for="state">State</label>
+            <input
+              id="state"
+              v-model="state"
+              type="text"
+              class="form-control"
+              placeholder="State"
+              :disabled="searchQueryEnabled"
+            />
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-3">
+            <label for="postalcode">Postal Code</label>
+            <input
+              id="postalcode"
+              v-model="postalCode"
+              type="text"
+              class="form-control"
+              placeholder="Postal Code"
+              :disabled="searchQueryEnabled"
+            />
+          </div>
+          <div class="col-md-3">
+            <label for="country">Country</label>
+            <input
+              id="country"
+              v-model="country"
+              type="text"
+              class="form-control"
+              placeholder="Country"
+              :disabled="searchQueryEnabled"
+            />
+          </div>
+        </div>
+        <div class="row justify-content-center mt-3">
+          <!-- Login button that is only enabled if both email and password fields are empty -->
+          <button
+            type="button"
+            class="col-3 btn btn-outline-info"
+            @click="searchQueryEnabled = !searchQueryEnabled"
+          >
+            <span v-if="searchQueryEnabled">Edit Manually</span>
+            <span v-else>Use Autofill/Search</span>
+          </button>
+        </div>
+        <div class="row justify-content-center mt-3">
+          <button
+            type="button"
+            class="col-3 btn btn-outline-danger"
+            @click="page = 1"
+          >
+            <span>Go Back</span>
+          </button>
+        </div>
+        <div class="row justify-content-center mt-3">
+          <button type="button" class="col-3 btn btn-primary" @click="submit">
+            Submit
+          </button>
+        </div>
       </div>
     </form>
   </div>
@@ -134,38 +220,93 @@
 export default {
   data() {
     return {
-      email: "", // Data property for the email entered
-      password: "", // Data property for the password entered
-      firstname: "",
-      lastname: "",
-      dob: "",
+      page: 1,
+      email: '', // Data property for the email entered
+      password: '', // Data property for the password entered
+      firstname: '',
+      lastname: '',
+      dob: '',
+      searchQuery: '',
+      searchQueryEnabled: true,
+      street: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      country: '',
       blur: false,
     }
   },
+  watch: {
+    searchQuery(val) {
+      if (val.length >= 3) {
+        this.populateSearchData(val)
+      }
+    },
+  },
   methods: {
+    async populateSearchData(val) {
+      const data = await fetch(
+        `https://autocomplete.geocoder.ls.hereapi.com/6.2/suggest.json?apiKey=${process.env.VUE_APP_HERE_API_KEY}&maxresults=1&query=${val}`
+      )
+      const result = await data.json()
+      if (result.suggestions && result.suggestions.length > 0) {
+        if (
+          result.suggestions[0].address.houseNumber &&
+          result.suggestions[0].address.street
+        ) {
+          this.street =
+            result.suggestions[0].address.houseNumber +
+            ' ' +
+            result.suggestions[0].address.street
+        } else {
+          this.street = ''
+        }
+        this.city = result.suggestions[0].address.city
+          ? result.suggestions[0].address.city
+          : ''
+        this.state = result.suggestions[0].address.state
+          ? result.suggestions[0].address.state
+          : ''
+        this.postalCode = result.suggestions[0].address.postalCode
+          ? result.suggestions[0].address.postalCode
+          : ''
+        this.country = result.suggestions[0].address.country
+          ? result.suggestions[0].address.country
+          : ''
+      } else {
+        this.street = ''
+        this.city = ''
+        this.state = ''
+        this.postalCode = ''
+        this.country = ''
+      }
+    },
     getMaxDate() {
       // This function is used to limit the DOB to a maximum value of current date/today's date.
-      const date = new Date();
+      const date = new Date()
       // date.setFullYear(date.getFullYear() - 18);
-      return date.toISOString().split("T")[0];
+      return date.toISOString().split('T')[0]
     },
     openPage() {
-      this.$router.push({ path: "login" }); // Switch view to Login.vue
+      this.$router.push({ path: 'login' }) // Switch view to Login.vue
     },
-        submit() {
-      this.blur = true;
+    submit() {
+      this.blur = true
       if (this.validate().result) {
-        alert("Done");
+        alert('Done')
       } else {
-        alert("Not Done");
+        alert('Not Done')
+      }
+      if (this.page === 1) {
+        this.page = 2
       }
     },
     validate() {
-      const firstnameValidation = this.firstname.length;
+      const firstnameValidation = this.firstname.length
       const lastnameValidation = this.lastname.length
-      const emailValidation = this.email.length;
-      const passwordValidation = this.password.length >= 5;
-      const dobValidation = ""
+      const emailValidation = this.email.length
+      const passwordValidation = this.password.length >= 5
+      const dobValidation = ''
       // const nameValidation = this.
       return {
         firstname: firstnameValidation,
@@ -173,9 +314,14 @@ export default {
         email: emailValidation,
         password: passwordValidation,
         dob: dobValidation,
-        result: firstnameValidation && lastnameValidation && emailValidation && passwordValidation && dobValidation,
-      };
+        result:
+          firstnameValidation &&
+          lastnameValidation &&
+          emailValidation &&
+          passwordValidation &&
+          dobValidation,
+      }
     },
   },
-};
+}
 </script>
