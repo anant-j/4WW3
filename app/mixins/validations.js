d7208d11693fececa41cd040f0b0fcd91794fa08
khannk1 <35265519+khannk1@users.noreply.github.com>
2021-10-30 23:42:29 -0400

    Adding validation for add page (#9)

--- app/mixins/validations.js
+++ app/mixins/validations.js
@@ -22,16 +22,17 @@ export default {
         },
         // TODO
         validateName(name) {
-
+            return name && name.trim()
         },
         validateDescription(description) {
-
+            return ((description.split(/\b(\s)/)).length >= 20)
         },
         validateLatitudeLongitude(coordinate) {
 
         },
         validateWebsite(website) {
-
+            const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00A1-\uFFFF0-9]-*)*[a-z\u00A1-\uFFFF0-9]+)(?:\.(?:[a-z\u00A1-\uFFFF0-9]-*)*[a-z\u00A1-\uFFFF0-9]+)*(?:\.(?:[a-z\u00A1-\uFFFF]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+            return regexp.test(website);
         }
 
     },
