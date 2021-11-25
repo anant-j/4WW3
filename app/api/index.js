6a3d68b98684c90d9ccaf91e6ebe752a6dfadc53
kuber khanna <khannakuber14@gmail.com>
2021-11-25 14:53:23 -0500

    Uploading new API

--- app/api/index.js
+++ app/api/index.js
@@ -5,8 +5,8 @@ const rateLimit = require("express-rate-limit");
 
 
 const apiLimiter = rateLimit({
-  windowMs: 10 * 1000, // 15 minutes
-  max: 3 // limit each IP to 100 requests per windowMs
+    windowMs: 10 * 1000, // 15 minutes
+    max: 3 // limit each IP to 100 requests per windowMs
 });
 
 
@@ -16,33 +16,33 @@ const saltRounds = 10
 const jwt = require("jsonwebtoken");
 
 const connectionSetup = {
-  host: process.env.VUE_APP_SQL_HOST,
-  port: process.env.VUE_APP_SQL_PORT,
-  user: process.env.VUE_APP_SQL_USER,
-  password: process.env.VUE_APP_SQL_PASSWORD,
-  database: process.env.VUE_APP_SQL_DATABASE,
+    host: process.env.VUE_APP_SQL_HOST,
+    port: process.env.VUE_APP_SQL_PORT,
+    user: process.env.VUE_APP_SQL_USER,
+    password: process.env.VUE_APP_SQL_PASSWORD,
+    database: process.env.VUE_APP_SQL_DATABASE,
 }
 
 class User {
-  constructor(email, firstName, lastName, dob, latitude, longitude) {
-    this.email = email;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.dob = dob;
-    this.latitude = latitude;
-    this.longitude = longitude;
-  }
-
-  get json() {
-    return {
-      email: this.email,
-      firstName: this.firstName,
-      lastName: this.lastName,
-      dob: this.dob,
-      latitude: this.latitude,
-      longitude: this.longitude
+    constructor(email, firstName, lastName, dob, latitude, longitude) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
-  }
+
+    get json() {
+        return {
+            email: this.email,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            dob: this.dob,
+            latitude: this.latitude,
+            longitude: this.longitude
+        }
+    }
 }
 
 app.use(json())
@@ -50,209 +50,225 @@ app.use(helmet())
 app.use(apiLimiter);
 
 app.get('/ping', (req, res) => {
-  res.send('UP');
+    res.send('UP');
 });
 
-app.post('/login', async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const connection = await mysql.createConnection(connectionSetup)
-  try {
-    if (!email) {
-      return res.status(400).send({
-        error: 'Email is required'
-      })
+app.post('/login', async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const connection = await mysql.createConnection(connectionSetup)
+    try {
+        if (!email) {
+            return res.status(400).send({
+                error: 'Email is required'
+            })
+        }
+        if (!password) {
+            return res.status(400).send({
+                error: 'Password is required'
+            })
+        }
+        const [rows] = await connection.execute(
+            'SELECT * FROM Users WHERE EMAIL = ?', [email]
+        )
+        if (rows.length) {
+            const passwordVerified = await checkPassword(password, rows[0].Password);
+            if (passwordVerified) {
+                const firstName = rows[0].FirstName;
+                const lastName = rows[0].LastName;
+                const dob = rows[0].DOB;
+                const latitude = rows[0].Latitude;
+                const longitude = rows[0].Longitude;
+                const user = new User(email, firstName, lastName, dob, latitude, longitude);
+                res.send({
+                    success: true,
+                    user,
+                    JWT: generateJWTToken(user),
+                })
+            } else {
+                res.send({
+                    success: false,
+                    errorCode: 'password',
+                })
+            }
+        } else {
+            res.send({
+                success: false,
+                errorCode: 'email',
+            })
+        }
+    } catch (error) {
+        res.send({
+            success: false,
+            errorCode: 'unknown',
+        })
     }
-    if (!password) {
-      return res.status(400).send({
-        error: 'Password is required'
-      })
-    }
-    const [rows] = await connection.execute(
-      'SELECT * FROM Users WHERE EMAIL = ?',
-      [email]
-    )
-    if (rows.length) {
-      const passwordVerified = await checkPassword(password, rows[0].Password);
-      if (passwordVerified) {
-        const firstName = rows[0].FirstName;
-        const lastName = rows[0].LastName;
-        const dob = rows[0].DOB;
-        const latitude = rows[0].Latitude;
-        const longitude = rows[0].Longitude;
+})
+
+app.post('/register', async(req, res) => {
+    const email = req.body.email;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const dob = req.body.dob;
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    const connection = await mysql.createConnection(connectionSetup)
+    const hashedPassword = await hashPassword(req.body.password);
+    try {
+        const [rows] = await connection.query(
+            'INSERT INTO Users (Email, Password, FirstName, LastName, DOB, Latitude, Longitude) VALUES (?,?,?,?,?,?,?)', [email, hashedPassword, firstName, lastName, dob, latitude, longitude]
+        )
         const user = new User(email, firstName, lastName, dob, latitude, longitude);
-        res.send({
-          success: true,
-          user,
-          JWT: generateJWTToken(user),
-        })
-      } else {
-        res.send({
-          success: false,
-          errorCode: 'password',
-        })
-      }
-    } else {
-      res.send({
-        success: false,
-        errorCode: 'email',
-      })
+        if (rows.affectedRows === 1) {
+            res.send({
+                success: true,
+                user,
+                JWT: generateJWTToken(user),
+            })
+        } else {
+            res.send({
+                success: false,
+                errorCode: 'unknown',
+            })
+        }
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            res.send({
+                success: false,
+                errorCode: 'email',
+            })
+        } else {
+            res.send({
+                success: false,
+                errorCode: 'unknown',
+            })
+        }
     }
-  } catch (error) {
-    res.send({
-      success: false,
-      errorCode: 'unknown',
-    })
-  }
 })
 
-app.post('/register', async (req, res) => {
-  const email = req.body.email;
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const dob = req.body.dob;
-  const latitude = req.body.latitude;
-  const longitude = req.body.longitude;
-  const connection = await mysql.createConnection(connectionSetup)
-  const hashedPassword = await hashPassword(req.body.password);
-  try {
-    const [rows] = await connection.query(
-      'INSERT INTO Users (Email, Password, FirstName, LastName, DOB, Latitude, Longitude) VALUES (?,?,?,?,?,?,?)',
-      [email, hashedPassword, firstName, lastName, dob, latitude, longitude]
-    )
-    const user = new User(email, firstName, lastName, dob, latitude, longitude);
-    if (rows.affectedRows === 1) {
-      res.send({
-        success: true,
-        user,
-        JWT: generateJWTToken(user),
-      })
+app.post('/addRestaurant', async(req, res) => {
+    const name = req.body.name;
+    const phone = req.body.phone;
+    const website = req.body.website;
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    const description = req.body.description;
+    const image = req.body.image;
+    const connection = await mysql.createConnection(connectionSetup)
+    try {
+        const [rows] = await connection.query(
+            'INSERT INTO Restaurants (Name, About, Phone, Website, Image, Latitude, Longitude) VALUES (?,?,?,?,?,?,?)', [name, description, phone, website, image, latitude, longitude]
+        )
+        if (rows.affectedRows === 1) {
+            res.send({
+                success: true,
+                id: rows.insertId,
+            })
+        } else {
+            res.send({
+                success: false,
+                errorCode: 'unknown',
+            })
+        }
+    } catch (error) {
+        res.send({
+            success: false,
+            errorCode: 'unknown',
+        })
     }
-    else {
-      res.send({
-        success: false,
-        errorCode: 'unknown',
-      })
-    }
-  } catch (error) {
-    if (error.code === 'ER_DUP_ENTRY') {
-      res.send({
-        success: false,
-        errorCode: 'email',
-      })
-    }
-    else {
-      res.send({
-        success: false,
-        errorCode: 'unknown',
-      })
-    }
-  }
-})
-
-app.post('/addRestaurant', async (req, res) => {
-  const name = req.body.name;
-  const phone = req.body.phone;
-  const website = req.body.website;
-  const latitude = req.body.latitude;
-  const longitude = req.body.longitude;
-  const description = req.body.description;
-  const image = req.body.image;
-  const connection = await mysql.createConnection(connectionSetup)
-  try {
-    const [rows] = await connection.query(
-      'INSERT INTO Restaurants (Name, About, Phone, Website, Image, Latitude, Longitude) VALUES (?,?,?,?,?,?,?)',
-      [name, description, phone, website, image, latitude, longitude]
-    )
-    if (rows.affectedRows === 1) {
-      res.send({
-        success: true,
-        id: rows.insertId,
-      })
-    }
-    else {
-      res.send({
-        success: false,
-        errorCode: 'unknown',
-      })
-    }
-  }
-  catch (error) {
-    res.send({
-      success: false,
-      errorCode: 'unknown',
-    })
-  }
 })
 
 app.get('/getRestaurant', async(req, res) => {
-  const id = req.query.id;
-  const connection = await mysql.createConnection(connectionSetup)
-  try {
-    const [rows] = await connection.execute(
-      'SELECT * FROM Restaurants WHERE ID = ?',
-      [id]
-    )
-    if (rows.length) {
-      res.send({
-        success: true,
-        restaurant: rows[0],
-      })
+    const id = req.query.id;
+    const connection = await mysql.createConnection(connectionSetup)
+    try {
+        const [rows] = await connection.execute(
+            'SELECT * FROM Restaurants WHERE ID = ?', [id]
+        )
+        if (rows.length) {
+            res.send({
+                success: true,
+                restaurant: rows[0],
+            })
+        } else {
+            res.send({
+                success: false,
+                errorCode: 'notFound',
+            })
+        }
+    } catch (error) {
+        res.send({
+            success: false,
+            errorCode: 'unknown',
+        })
     }
-    else {
-      res.send({
-        success: false,
-        errorCode: 'notFound',
-      })
+})
+
+app.get('/getRestaurantByName', async(req, res) => {
+    const name = req.query.name;
+    const connection = await mysql.createConnection(connectionSetup)
+    try {
+        const [rows] = await connection.execute(
+            'SELECT * FROM Restaurant WHERE NAME LIKE concat(?, ' % ')', [name]
+        )
+        if (rows.length) {
+            res.send({
+                success: true,
+                restaurant: rows[0],
+            })
+        } else {
+            res.send({
+                success: false,
+                errorCode: 'notFound',
+            })
+        }
+    } catch (error) {
+        res.send({
+            success: false,
+            errorCode: 'unknown',
+        })
     }
-  }
-  catch (error) {
-    res.send({
-      success: false,
-      errorCode: 'unknown',
-    })
-  }
 })
 
 app.post('/verifyJWT', (req, res) => {
-  const decoded = verifyJWTToken(req.body.token);
-  // const providedEmail = req.body.email;
-  if (decoded
-    // && providedEmail === decoded.user.email
-  ) {
-    res.send({
-      success: true,
-      user: decoded.user,
-    })
-  } else {
-    res.send({
-      success: false,
-    })
-  }
+    const decoded = verifyJWTToken(req.body.token);
+    // const providedEmail = req.body.email;
+    if (decoded
+        // && providedEmail === decoded.user.email
+    ) {
+        res.send({
+            success: true,
+            user: decoded.user,
+        })
+    } else {
+        res.send({
+            success: false,
+        })
+    }
 });
 
 export default app
 
 async function hashPassword(plainTextPassword) {
-  const hash = await bcrypt.hash(plainTextPassword, saltRounds)
-  return hash
+    const hash = await bcrypt.hash(plainTextPassword, saltRounds)
+    return hash
 }
 
 async function checkPassword(plainTextPassword, hash) {
-  const result = await bcrypt.compare(plainTextPassword, hash)
-  return result
+    const result = await bcrypt.compare(plainTextPassword, hash)
+    return result
 }
 
 function generateJWTToken(user) {
-  const token = jwt.sign({ user }, process.env.VUE_APP_JWT_SECRET, { expiresIn: '1h' });
-  return token
+    const token = jwt.sign({ user }, process.env.VUE_APP_JWT_SECRET, { expiresIn: '1h' });
+    return token
 }
 
 function verifyJWTToken(token) {
-  try {
-    const decoded = jwt.verify(token, process.env.VUE_APP_JWT_SECRET);
-    return decoded;
-  } catch (error) {
-    return null;
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.VUE_APP_JWT_SECRET);
+        return decoded;
+    } catch (error) {
+        return null;
+    }
 }
